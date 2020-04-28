030e5b5db3a6665571ed0c99b66ed8fdeb68f423
Peter Tenghamn <petertenghamn@hotmail.com>
2020-04-28 11:19:01 +0200

    Variable class holders

---
+++ src/Data/GraphedData.java
@@ -0,0 +1,10 @@
+package Data;
+
+import java.util.ArrayList;
+
+public class GraphedData {
+
+    //list for stored values on each iteration (can be used to graph the result). An average case version of this will be stored in the DB as well.
+    private ArrayList<SimVariables> variables;
+
+}
---
+++ src/Data/SimVariables.java
@@ -0,0 +1,7 @@
+package Data;
+
+public class SimVariables {
+
+    //should contain the 3 variables used in the algorithms and be passed between classes
+
+}
