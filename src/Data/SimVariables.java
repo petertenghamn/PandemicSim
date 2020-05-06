73fe5550baec46484da2fbd80169be290e168ea6
Öjvind Nilsson <ojvindnilsson@gmail.com>
2020-05-06 13:26:35 +0200

    Skeleton Complete
    Uses the live cycle of a grass and it's reproduction in order to create a base for future developments.
    
    DynamicAlgorithm takes in a SimVariables and updates it then returns the SimVariables Object

--- src/Data/SimVariables.java
+++ src/Data/SimVariables.java
@@ -4,16 +4,16 @@ public class SimVariables{
 
     public int foxes;
     public int bunnies;
-    public int plants;
+    public int grass;
 
     public SimVariables(){
 
     }
 
-    public SimVariables(int plants, int bunnies, int foxes){
+    public SimVariables(int grass, int bunnies, int foxes){
         this.foxes = foxes;
         this.bunnies = bunnies;
-        this.plants = plants;
+        this.grass = grass;
     }
 
 }
